c5a478a981e739a48f4d2f0a38c83932edc84593
Mohammed khaled <qavento.ltd@gmail.com>
2025-09-25 01:24:01 +0300

    Update menu.js

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -6,9 +6,9 @@ nav.style.marginLeft = '6000px';
 menu.onclick = () => {
     if (nav.style.marginLeft === "6000px") {
       nav.style.marginLeft = '60px';
-      menu.src = '/public/icon/clear.svg';
+      menu.src = './public/icon/clear.svg';
     } else {
       nav.style.marginLeft = '6000px';
-      menu.src = '/public/icon/menu.svg'
+      menu.src = './public/icon/menu.svg'
     }
 }
