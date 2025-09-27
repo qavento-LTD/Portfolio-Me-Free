d8b7069c7bcf6adb542f7871077b2cb67ae3a655
mohammed khaled <qavento.ltd@gmail.com>
2025-09-27 05:10:22 +0300

    update

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,14 +1,21 @@
 let menu = document.querySelector('.menu');
 let nav = document.querySelector('nav');
+let links = document.querySelectorAll('.link a');
 
 
 nav.style.marginLeft = '6000px';
 menu.onclick = () => {
     if (nav.style.marginLeft === "6000px") {
       nav.style.marginLeft = '60px';
-      menu.src = '../public/icon/clear.svg';
+      menu.src = 'public/icon/clear.svg';
     } else {
       nav.style.marginLeft = '6000px';
-      menu.src = '../public/icon/menu.svg';
+      menu.src = 'public/icon/menu.svg';
     }
 }
+links.forEach(link => {
+    link.onclick = () => {
+        nav.style.marginLeft = '6000px';
+        menu.src = 'public/icon/menu.svg';
+    } 
+});
